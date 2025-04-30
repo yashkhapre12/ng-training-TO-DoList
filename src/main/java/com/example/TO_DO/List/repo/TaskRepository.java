69b06542b6038748dff45ed62310ffbce834e165
yash khapre <93426045+Yashkha@users.noreply.github.com>
2025-04-30 14:52:30 +0530

    Remove unnecessary files and keep only frontend and backend folders

--- src/main/java/com/example/TO_DO/List/repo/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.TO_DO.List.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.TO_DO.List.entity.Task;
-
-public interface TaskRepository extends JpaRepository<Task, Long> {
-}
-
